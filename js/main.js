ac6476479b55a92d7d59f7191c1bb3809cb9ee55
benjaehyun <benjaminjaelee@gmail.com>
2023-08-07 10:43:04 -0700

    build board array and rendering grid divs

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 	/*----- constants -----*/
-const easyMode = {
+const difficultyMode = [{
 	gridWidth: 9, 
 	gridHeight: 9, 
 	bombNumber: 10
-}; 
-
-const mediumMode = {
+	}, 
+ 	{
 	gridWidth: 16, 
 	gridHeight: 16, 
 	bombNumber: 40
-}; 
-
-const hardMode = {
+	},
+	{
 	gridWidth: 30, 
 	gridHeight: 16, 
 	bombNumber: 99
-};
+	}]
 	/*----- state variables -----*/
 let winner, bombCounter, timer ; 
-const board; 
+const boardArr = []; 
 
 	/*----- cached elements  -----*/
 const diffSelectorEl = document.querySelector("#difficulty-selector");
@@ -29,21 +27,40 @@ const boardEl = document.querySelector("#grid");
 diffSelectorEl.addEventListener("click", handleDifficultyClick);
 
 	/*----- functions -----*/
+init() 
+
+function init() {
+	board = [] 
+	boardEl.innerHTML = ""
+	winner = false 
+	bombCounter = 0 
+	timer = 0 
+	// renderDifficultySelection()
+}
+
+// function renderDifficultySelection() {
+// 	boardEl.style.display = "none"
+// 	diffSelectorEl.style.display = "flex"
+// }
+
 function handleDifficultyClick(evt) {
 	if (evt.target.id === "easy") {
-		gameStart("easy");
+		renderGameStart(0);
 	} else if (evt.target.id === "medium") {
-		gameStart("medium");
+		renderGameStart(1);
 	} else if (evt.target.id === "hard") {
-		gameStart("hard");
+		renderGameStart(2);
 	}
 }
 
 // later: change css for top bar elements to start hidden before changing the ~.style.display to the properties outlined in the current css file 
-function gameStart(difficulty) {
+function renderGameStart(difficulty) {
 	diffSelectorEl.style.display = "none";
 	boardEl.style.display = "grid";
-	buildArr(`${difficulty}Mode.gridWidth`, `${difficulty}Mode.gridHeight`);
+	console.log(difficultyMode[difficulty].gridWidth)
+	buildArr(difficultyMode[difficulty].gridWidth, difficultyMode[difficulty].gridHeight);
+	renderCells(difficultyMode[difficulty].gridWidth, difficultyMode[difficulty].gridHeight);
+
 	// board = new MinesweeperGame(difficulty) 
 }
 
@@ -61,22 +78,36 @@ function buildArr (x, y) {
 	const arrRows = []; 
 	for (i = 0; i < x ; i++) {
 		arrRows.push(new cellObject);
-	};
-	const arrColumns = []; 
+		// arrRows.push("1");
+	}; 
 	for (i = 0; i < y; i++) {
-		arrColumns.push(arrRows)
+		boardArr.push(arrRows)
 	}
+	return boardArr; 
 };
 
 class cellObject {
 	constructor() {
-		this.hasBomb: null, 
-		this.numOfNearBombs: null,
-		this.isFlipped: false, 
-		this.isFlagged: null 
+		this.hasBomb = null, 
+		this.numOfNearBombs = null,
+		this.isFlipped = false, 
+		this.isFlagged = false 
 	}
 }
 
+function renderCells ( gridWidth, gridHeight ) {
+	for (i = 0; i < gridWidth ; i ++){
+		for (e = 0; i < gridHeight; e ++) {
+			const cellEl = document.createElement("div")
+			cellEl.setAttribute("id", `r${i}c${e}`)
+			boardEl.appendChild(cellEl)
+		}
+	}
+}
+
+function getLoser() {			// check if player clicked on bomb 
+
+}
 
 // class MinesweeperGame {
 // 	constructor (difficulty) {
