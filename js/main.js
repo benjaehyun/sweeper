1479772eb402e54b77cce98a19cdd7e916351f5b
benjaehyun <benjaminjaelee@gmail.com>
2023-08-08 15:09:03 -0700

    added the beginnings for the flood fill recursive function

--- js/main.js
+++ js/main.js
@@ -28,6 +28,8 @@ const boardEl = document.querySelector("#grid");
 diffSelectorEl.addEventListener("click", handleDifficultyClick);
 boardEl.addEventListener("click", handleBoardClick)
 boardEl.addEventListener("contextmenu", handleRightClick)
+// boardEl.on('mouseup', handleMiddleClick)
+// boardEl.addEventListener("wheel", (event) => {console.log('scrollwheel clicked')});
 
 	/*----- functions -----*/
 init() 
@@ -60,20 +62,33 @@ function handleDifficultyClick(evt) {
 }
 
 function handleBoardClick(evt) {
-	const gridEls = [...document.querySelectorAll('#grid>div')]
-	const colIdx = ( gridEls.indexOf(evt.target) % difficultyMode[difficulty].gridWidth )
-	const rowIdx = Math.floor( gridEls.indexOf(evt.target) / difficultyMode[difficulty].gridHeight)
-	if (firstClick === true) {
-		placeRandomBomb(difficultyMode[difficulty].bombNumber, rowIdx, colIdx); 
-		calculateNearBombs(); 
-		firstClick = false;
-		renderCellContent(colIdx, rowIdx);
-	} else {
-		renderCellContent(colIdx, rowIdx);
-		console.log(colIdx, rowIdx)
-		// all of the other clicks to play the game 
-	}
+	if (evt.button === 0 ) {
+		const gridEls = [...document.querySelectorAll('#grid>div')]
+		const colIdx = ( gridEls.indexOf(evt.target) % difficultyMode[difficulty].gridWidth )
+		const rowIdx = Math.floor( gridEls.indexOf(evt.target) / difficultyMode[difficulty].gridHeight)
+		if (firstClick === true) {
+			placeRandomBomb(difficultyMode[difficulty].bombNumber, rowIdx, colIdx); 
+			calculateNearBombs(); 
+			renderCellContent(colIdx, rowIdx);
+			floodFillAll(colIdx, rowIdx)
+			// floodFillUp(colIdx, rowIdx)
+			// floodFillDown (colIdx, rowIdx)
+			// floodFillLeft (colIdx, rowIdx)
+			// floodFillRight (colIdx, rowIdx)
+			// floodFillTopLeft (colIdx, rowIdx)
+			// floodFillTopRight (colIdx, rowIdx)
+			// floodFillBottomLeft (colIdx, rowIdx)
+			// floodFillBottomRight (colIdx, rowIdx)
+			// still need to implement the flood fill algorithm
+		} else {
+			renderCellContent(colIdx, rowIdx);
+			console.log(colIdx, rowIdx)
+			// all of the other clicks to play the game 
+		}
 	// console.log(colIdx, rowIdx, difficultyMode[difficulty].bombNumber)
+	} else if (evt.button === 4) {  /* still not working */
+		console.log("middlemouseclick")
+	}
 }
 
 function handleRightClick(evt) {  /* STILL NEED TO MAKE THIS A TOGGLE */
@@ -82,9 +97,14 @@ function handleRightClick(evt) {  /* STILL NEED TO MAKE THIS A TOGGLE */
 		const gridEls = [...document.querySelectorAll('#grid>div')]
 		const colIdx = ( gridEls.indexOf(evt.target) % difficultyMode[difficulty].gridWidth )
 		const rowIdx = Math.floor( gridEls.indexOf(evt.target) / difficultyMode[difficulty].gridHeight)
+		boardArr
 		document.getElementById(`c${rowIdx}r${colIdx}`).innerText = "🚩"
 	} else return; 
+}
 
+function handleMiddleClick (evt) {  /* still not working */
+	// evt.preventDefault() 
+	console.log("middle mouse click")
 }
 
 // later: change css for top bar elements to start hidden before changing the ~.style.display to the properties outlined in the current css file 
@@ -100,11 +120,12 @@ function renderGameStart(difficulty) {
 }
 
 function renderCellContent (i, j) {
-		if (boardArr[i][j].hasBomb === true) {
-			document.getElementById(`c${j}r${i}`).innerText = "💣"
-		} else {
-			document.getElementById(`c${j}r${i}`).innerHTML = `<strong>${boardArr[i][j].numOfNearBombs}</strong>`
-		}
+	boardArr[i][j].isFlipped = true; 
+	if (boardArr[i][j].hasBomb === true) {
+		document.getElementById(`c${j}r${i}`).innerText = "💣"
+	} else {
+		document.getElementById(`c${j}r${i}`).innerHTML = `<strong>${boardArr[i][j].numOfNearBombs}</strong>`
+	}
 }
 
 function renderAllCellsContent () {
@@ -152,14 +173,15 @@ class CellObject {
 		this.hasBomb = false, 
 		this.numOfNearBombs = 0,
 		this.isFlipped = false, 
-		this.isFlagged = false 
+		this.isFlagged = false,
+		this.flagCounter = 0
 	}
 }
 
 function renderCells (height, width) {
 	for (let i = 0; i < height; i ++) {
 		for (let e = 0; e < width; e ++) {
-			const cellEl = document.createElement("div")
+			const cellEl = document.createElement("div") /* changed from div to button*/ 
 			cellEl.setAttribute("id", `c${i}r${e}`)
 			cellEl.innerText = ""
 			cellEl.style.border = "solid"
@@ -167,7 +189,6 @@ function renderCells (height, width) {
 			cellEl.style.justifyContent = "center"
 			cellEl.style.alignItems = "center"
 			boardEl.appendChild(cellEl)
-			
 		}
 	}
 	boardEl.style.gridTemplateColumns = `repeat(${width}, 1fr)`
@@ -181,6 +202,7 @@ function placeRandomBomb(bombNumber, rowIdx, colIdx) {
 		let bombCol = randomCol(colIdx); 
 		if (boardArr[bombCol][bombRow].hasBomb === false) {
 			boardArr[bombCol][bombRow].hasBomb = true; 
+			boardArr[bombCol][bombRow].numOfNearBombs = null; 
 			i++; 
 		} 
 	}	
@@ -199,24 +221,18 @@ function calculateNearBombs() {
 	}
 }
 
-	/* i dont think that you can use a for loop because there are edge cases where it can produce the same cell location*/
-	// for (let i = 0; i < bombNumber; i++) {
-	// 	boardArr[randomRow(rowIdx)][randomCol(colIdx)].hasBomb = true; 
-	// 	// randomRow(rowIdx)
-	// 	console.log(randomRow(rowIdx), randomCol(colIdx))
-	// }
-	
-
 function randomRow(rowIdx) {
 	let randRow = null; 
-	while (randRow === null || randRow === rowIdx) {
+	// while (randRow === null || randRow === rowIdx || randRow === (rowIdx + 2) || randRow === (rowIdx - 2)) {		trying to force a 0 bomb count for the starting square 
+	while (randRow === null || randRow === rowIdx ) {
 		randRow = Math.round(Math.random() * (difficultyMode[difficulty].gridWidth - 1) ) 
 	}
 	return randRow; 
 }
 function randomCol(colIdx) {
 	let randCol = null; 
-	while (randCol === null || randCol === colIdx) {
+	// while (randCol === null || randCol === colIdx || randCol === (colIdx + 2) || randCol === (colIdx - 2)) {		trying to force a 0 bomb count for the starting square 
+	while (randCol === null || randCol === colIdx ) {
 		randCol = Math.round(Math.random() * (difficultyMode[difficulty].gridHeight - 1) ) 
 	}
 	return randCol; 
@@ -272,6 +288,252 @@ function checkSides(count, i, j) {
 function getLoser() {			// check if player clicked on bomb 
 
 }
+
+function floodFillAll (i,j) {
+	if (firstClick === true){
+		clearAbove(i,j)
+		clearBelow(i,j)
+		clearRight(i,j)
+		clearLeft(i,j)
+		clearTopLeft(i,j)
+		clearTopRight(i,j)
+		clearBottomLeft(i,j)
+		clearBottomRight(i,j)
+		firstClick = false; 
+	}
+	if(boardArr[i] && boardArr[i][j]) {
+		if (boardArr[i][j].isFlipped === false){
+			clearAbove(i,j)
+			clearBelow(i,j)
+			clearRight(i,j)
+			clearLeft(i,j)
+			clearTopLeft(i,j)
+			clearTopRight(i,j)
+			clearBottomLeft(i,j)
+			clearBottomRight(i,j)
+		}
+	} else return
+
+}
+
+
+function floodFillUp (i, j) {
+	if (boardArr[i] && boardArr[i][j]){
+		clearAbove(i,j)
+		return floodFillUp(i, j-1)
+	}
+}
+function floodFillDown (i, j) {
+	if (boardArr[i] && boardArr[i][j]){
+		clearBelow(i,j)
+		return floodFillDown(i, j+1)
+	}
+}
+function floodFillRight (i, j) {
+	if (boardArr[i] && boardArr[i][j]){
+		clearRight(i,j)
+		return floodFillRight(i+1, j)
+	}
+}
+function floodFillLeft (i, j) {
+	if (boardArr[i] && boardArr[i][j]){
+		clearLeft(i,j)
+		return floodFillLeft(i-1, j)
+	}
+}
+function floodFillTopLeft (i, j) {
+	if (boardArr[i] && boardArr[i][j]){
+		clearTopLeft(i,j)
+		return floodFillTopLeft(i-1, j-1)
+	}
+}
+function floodFillTopRight (i, j) {
+	if (boardArr[i] && boardArr[i][j]){
+		clearLeft(i,j)
+		return floodFillTopRight(i+1, j-1)
+	}
+}
+function floodFillBottomLeft (i, j) {
+	if (boardArr[i] && boardArr[i][j]){
+		clearLeft(i,j)
+		return floodFillBottomLeft(i-1, j+1)
+	}
+}
+function floodFillBottomRight (i, j) {
+	if (boardArr[i] && boardArr[i][j]){
+		clearLeft(i,j)
+		return floodFillBottomRight(i+1, j+1)
+	}
+}
+
+
+// function floodFill (i, j) {
+// 	if (i > (difficultyMode[difficulty].gridWidth - 1) || i < 0) {
+// 		return
+// 	}
+// 	if (j > (difficultyMode[difficulty].gridHeight - 1) || j < 0) {
+// 		return
+// 	}
+// 	if (boardArr[i][j].hasBomb === false) {
+// 		renderCellContent(i,j)
+// 		floodFill (i, j + 1)
+// 		floodFill (i, j - 1)
+// 		floodFill (i + 1, j)
+// 		floodFill (i - 1, j)
+// 		// if (i !== (difficultyMode[difficulty].gridHeight - 1)) {
+// 		// 	floodFill (i + 1, j)
+// 		// }
+// 		// if (i > 0) {
+// 		// 	floodFill (i - 1, j)
+// 		// }
+// 		// if (j !== (difficultyMode[difficulty].gridWidth - 1)) {
+// 		// 	floodFill (i, j + 1)
+// 		// }
+// 		// if (j > 0) {
+// 		// 	floodFill (i, j - 1)
+// 		// }
+// 	} else {return}
+// } 
+
+// recursive strategy 
+//  employ same check logic as above looking for a bomb 
+// guard against edge to determine which cells are available to be checked 
+	// THEN start branching statements checking for a bomb in the available cells 
+		// under those branching statements reveal the OTHER non-bomb containing, available cells 
+// do this in each direction 
+// probably define all of this as a HELPER RECURSIVE function that is called in the nonrecursive parent function 
+// question is to pass what function into what, starting from that first click square 
+
+// function clearAbove(i, j) {
+// 	if (j !== 0) {		/* if cell is on top row */
+// 		if (i === 0) {		/* if cell is on the far left */
+// 			if (boardArr[i][j-1].hasBomb === false) {
+// 				renderCellContent(i, j-1)				
+// 			}	
+// 			if (boardArr[i+1][j-1].hasBomb === false) {
+// 				renderCellContent(i+1, j-1)		
+// 			}		
+// 		} if (i === (difficultyMode[difficulty].gridWidth - 1)) {		/* if cell is on far right */
+// 			if (boardArr[i-1][j-1].hasBomb === false) {
+// 				renderCellContent(i-1, j-1)
+// 			}	
+// 			if (boardArr[i][j-1].hasBomb === false) {
+// 				renderCellContent(i, j-1)				
+// 			}	
+// 		} else {
+// 			if (boardArr[i-1][j-1].hasBomb === false) {
+// 				renderCellContent(i-1, j-1)
+// 			}	
+// 			if (boardArr[i][j-1].hasBomb === false) {
+// 				renderCellContent(i, j-1)				
+// 			}	
+// 			if (boardArr[i+1][j-1].hasBomb === false) {
+// 				renderCellContent(i+1, j-1)		
+// 			}	
+// 		}
+// 	}
+// 	return 
+// }
+
+// function clearBelow(i, j) {
+// 	if (j !== (difficultyMode[difficulty].gridHeight - 1)) {		/* if cell is on bottom row */
+// 		if (i === 0) {		/* if cell is on the far left */
+// 			if (boardArr[i][j+1].hasBomb === false) {
+// 				renderCellContent(i, j+1)				
+// 			}	
+// 			if (boardArr[i+1][j+1].hasBomb === false) {
+// 				renderCellContent(i+1, j+1)		
+// 			}	
+// 		} if (i === (difficultyMode[difficulty].gridWidth - 1)) {		/* if cell is on far right */
+// 			if (boardArr[i-1][j+1].hasBomb === false) {
+// 				renderCellContent(i-1, j+1)
+// 			}	
+// 			if (boardArr[i][j+1].hasBomb === false) {
+// 				renderCellContent(i, j+1)				
+// 			}
+// 		} else {
+// 			if (boardArr[i-1][j+1].hasBomb === false) {
+// 				renderCellContent(i-1, j+1)
+// 			}	
+// 			if (boardArr[i][j+1].hasBomb === false) {
+// 				renderCellContent(i, j+1)				
+// 			}	
+// 			if (boardArr[i+1][j+1].hasBomb === false) {
+// 				renderCellContent(i+1, j+1)		
+// 			}	
+// 		}
+// 	}
+// 	return  
+// }
+
+
+function clearAbove(i, j) {
+	if (j !== 0 && boardArr[i][j-1].isFlipped === false) {		/* if cell is on top row */	
+		if (boardArr[i][j-1].hasBomb === false) {
+			renderCellContent(i, j-1)				
+		}
+	} return 
+}
+
+function clearBelow(i, j) {
+	if (j !== (difficultyMode[difficulty].gridHeight - 1)  && boardArr[i][j+1].isFlipped === false) {		/* if cell is on bottom row */
+		if (boardArr[i][j+1].hasBomb === false) {
+			renderCellContent(i, j+1)				
+		}
+	} return  
+}
+
+function clearTopRight(i, j) {
+	if (j !== 0 && i !== (difficultyMode[difficulty].gridWidth - 1) && boardArr[i+1][j-1].isFlipped === false) {		/* if cell is on bottom row */
+		if (boardArr[i+1][j-1].hasBomb === false) {
+			renderCellContent(i+1, j-1)				
+		}
+	} return  
+}
+function clearTopLeft(i, j) {
+	if (j !== 0 && i !== 0 && boardArr[i-1][j-1].isFlipped === false) {		/* if cell is on bottom row */
+		if (boardArr[i-1][j-1].hasBomb === false) {
+			renderCellContent(i-1, j-1)				
+		}
+	} return  
+}
+function clearBottomRight(i, j) {
+	if (j !== (difficultyMode[difficulty].gridHeight - 1) && i !== (difficultyMode[difficulty].gridWidth - 1) && boardArr[i+1][j+1].isFlipped === false) {		/* if cell is on bottom row */
+		if (boardArr[i+1][j+1].hasBomb === false) {
+			renderCellContent(i+1, j+1)				
+		}
+	} return  
+}
+function clearBottomLeft(i, j) {
+	if (j !== (difficultyMode[difficulty].gridHeight - 1) && i !== 0 && boardArr[i-1][j+1].isFlipped === false) {		/* if cell is on bottom row */
+		if (boardArr[i-1][j+1].hasBomb === false) {
+			renderCellContent(i-1, j+1)				
+		}
+	} return  
+}
+
+function clearRight(i, j) {
+	if (i !== (difficultyMode[difficulty].gridWidth - 1)) {		/* if cell is not on right */
+		if (boardArr[i+1][j].hasBomb === false) {
+			renderCellContent(i + 1, j)				
+		}	
+	}
+	return  
+}
+
+// check that the array element actually exists as the parameter 
+
+
+function clearLeft(i, j) {
+	if (i !== 0) { 		/* if the cell is not on left */
+		if (boardArr[i-1][j].hasBomb === false) {
+			renderCellContent(i - 1, j)				
+		}	
+	} 
+	return  
+}
+
+
 
 // class MinesweeperGame {
 // 	constructor (difficulty) {
